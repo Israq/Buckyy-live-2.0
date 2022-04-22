b8498c5e844c3374aa7bd18c53694a61eff2fd0b
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-04-22 17:00:47 +0600

    live

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #new entry
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
   'default': {
-  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  'ENGINE': 'django.db.backends.mysql',
   'NAME': 'buckpdht_buckyydatabase',
   'USER': 'buckpdht_israq',
   'PASSWORD': 'Buckyy@123',
@@ -127,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
